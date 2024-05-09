3eafd798ea8122ba38a7743483efca2d064aced9
abrown104 <anna-r-brown@uiowa.edu>
2024-05-08 23:33:43 -0500

    Updated new files

---
+++ rental-system/src/test/java/dev/rentranger/rentalsystem/RentalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rentranger.rentalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
