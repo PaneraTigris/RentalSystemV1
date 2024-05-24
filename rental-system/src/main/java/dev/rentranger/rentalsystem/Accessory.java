06641c0ad89f71aa510b141458cfb2fdfa04215e
abrown104 <anna-r-brown@uiowa.edu>
2024-05-24 10:59:39 -0500

    Fixed some errors with class mismatch

--- rental-system/src/main/java/dev/rentranger/rentalsystem/Accessory.java
+++ rental-system/src/main/java/dev/rentranger/rentalsystem/Accessory.java
@@ -1,5 +1,9 @@
 package dev.rentranger.rentalsystem;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.*;
 
 @Entity
