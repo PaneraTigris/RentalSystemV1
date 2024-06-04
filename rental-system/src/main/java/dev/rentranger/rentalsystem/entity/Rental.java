900d7f994f930a3ce95c8d77c6a0a9d54e3b29e0
abrown104 <anna-r-brown@uiowa.edu>
2024-06-03 20:09:04 -0500

    Updated entity Rental

--- rental-system/src/main/java/dev/rentranger/rentalsystem/entity/Rental.java
+++ rental-system/src/main/java/dev/rentranger/rentalsystem/entity/Rental.java
@@ -14,10 +14,42 @@ public class Rental {
     @JoinColumn(name = "camera_id")
     private Camera camera;
 
+    private boolean available;
     private String customerName;
     private LocalDate rentalDate;
     private LocalDate returnDate;
-    private double price;
 
     // Constructors, getters, and setters
+    public Rental() {}
+
+    public Rental(boolean available, String customerName, LocalDate rentalDate, LocalDate returnDate) {
+        this.available = available;
+        this.customerName = customerName;
+        this.rentalDate = rentalDate;
+        this.returnDate = returnDate;
+        this.camera = new Camera();
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {return customerName;}
+    public void setCustomerName(String customerName) {this.customerName = customerName;}
+
+    public LocalDate getRentalDate() {return rentalDate;}
+    public void setRentalDate(LocalDate rentalDate) {this.rentalDate = rentalDate;}
+
+    public LocalDate getReturnDate() {return returnDate;}
+    public void setReturnDate(LocalDate returnDate) {this.returnDate = returnDate;}
+
+    public boolean isAvailable() {
+        return available;
+    }
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
 }
