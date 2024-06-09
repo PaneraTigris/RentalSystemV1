26324809555320b4306d76fe54c99d3f503771d3
abrown104 <anna-r-brown@uiowa.edu>
2024-06-09 14:50:19 -0500

    Fixed password encoder bean, need to fix authen. manager bean

---
+++ rental-system/src/main/java/dev/rentranger/rentalsystem/config/AppConfig2.java
@@ -0,0 +1,22 @@
+package dev.rentranger.rentalsystem.config;
+
+import dev.rentranger.rentalsystem.service.UserService;
+import dev.rentranger.rentalsystem.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AppConfig2 {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserService userService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        return new UserService(userRepository, passwordEncoder);
+    }
+}
--- rental-system/src/main/java/dev/rentranger/rentalsystem/controller/Auth2Controller.java
+++ rental-system/src/main/java/dev/rentranger/rentalsystem/controller/Auth2Controller.java
@@ -6,8 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/auth")
-public class AuthController {
+@RequestMapping("/api/user")
+public class Auth2Controller {
 
     @Autowired
     private UserService userService;
